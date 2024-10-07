35cff5f7e3e52232525c0403b42fff7503316ae6
Martin Lindström <martin.lindstrom@litsec.se>
2024-10-07 14:58:32 +0200

    IS-11 Bumped version to 3.2.0-SNAPSHOT

--- src/main/java/se/swedenconnect/opensaml/eidas/LibraryVersion.java
+++ src/main/java/se/swedenconnect/opensaml/eidas/LibraryVersion.java
@@ -23,8 +23,8 @@ package se.swedenconnect.opensaml.eidas;
 public final class LibraryVersion {
 
   private static final int MAJOR = 3;
-  private static final int MINOR = 1;
-  private static final int PATCH = 1;
+  private static final int MINOR = 2;
+  private static final int PATCH = 0;
 
   /**
    * Global serialization value for library classes.
